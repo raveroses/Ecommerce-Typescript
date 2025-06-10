7eacb10f5cc17e19e61bb398027ce152fa42a070
rave-roses <odekunlewaris01@gmail.com>
2025-06-10 23:52:36 +0100

    added

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
 // https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"), // This makes @ point to src/
+      "@": resolve(__dirname, "src"), // This makes @ point to src/
     },
   },
 });
